c89520a34942474b584cdc6f794a8bbd06b81a49
K-Amarnath <85100911+K-Amarnath@users.noreply.github.com>
2021-07-02 10:08:28 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void swap_max(int a[], int x, int y) 
+{
+    int d = a[y], e = 0;
+    for (int i = y; i < x; i++) 
+    {
+        if (a[i] > d) 
+        {
+            d = a[i];
+            e = i;
+        }
+    }
+    if (d != a[y]) 
+    {
+        a[e] = a[y];
+        a[y] = d;
+    }
+}
+
+void ssort(int p[], int z) 
+{
+    for (int i = 0; i < m; i++) 
+    {
+        swap_max(p, m, i);
+    }
+}
